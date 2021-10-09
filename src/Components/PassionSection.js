b2ba3d4ee0d97a6cddfbfda203669a10d637f39b
fahimdipto <sultanparvez77.sp@gmail.com>
2021-10-09 23:18:21 +0600

    Background and asset change

--- src/Components/PassionSection.js
+++ src/Components/PassionSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import {InnerLayout} from '../styles/Layouts';
 import Title from '../Components/Title';
-import ServiceCard from '../Components/ServiceCard';
+import PassionCard from './PassionCard';
 import design from '../img/design.svg';
 import intelligence from '../img/intelligence.svg';
 import gamedev from '../img/game-dev.svg';
@@ -13,13 +13,13 @@ function ServicesSection() {
             <ServicesSectionStyled>
                 <Title title={'Passion'} span={'Passion'} />
                 <div className="services">
-                    <ServiceCard 
+                    <PassionCard
                         image={design} 
                         title={'Web Development'}
                         paragraph={'I enjoy developing interactive UI and building complex modules. It is fascinating when we can design something which is already in our mind. When I am building an API for the backend it feels like solving a really interesting puzzle. And working on the frontend gives the pleasure of creating something.'}
                     />
                     <div className="mid-card">
-                        <ServiceCard 
+                        <PassionCard
                             image={intelligence} 
                             title={'Machine Learning'}
                             paragraph={"Machine learning can do wonder. Whether it is predicting a classification problem or simulating a scenario that is impossible to simulate otherwise, it always amaze me. I am a fan Reinforcement Learning. I have explored with methods like a2c and it was an wonderful learning. I will be really  thrilled if I get more opportunity to work in this sector. "
@@ -27,7 +27,7 @@ function ServicesSection() {
 
                         />
                     </div>
-                    <ServiceCard 
+                    <PassionCard
                         image={gamedev} 
                         title={'Gaming'}
                         paragraph={'Video games has been my favourite go to pass time. Enjoy playing fps shooters like CS:GO and Valorant'}
